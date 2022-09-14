228df6df5cba41610d4dfc64b78e0c9e46f2725b
melashu <meshu102@outlook.com>
2022-09-14 14:55:37 +0300

    Add TodoInut container

---
+++ src/compontes/TodoInput.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+import style from "../css/TodoInput.module.css";
+import { IoMdAddCircle } from "react-icons/io";
+
+const TodoInput = (props) => {
+  const [todoTitle, setTodoTitle] = useState({ title: "rr" });
+  const [title, setTitle] = useState("");
+
+  function addTodoHUndler(e) {
+    e.preventDefault();
+    props.addTodoItem(title);
+  }
+
+  const updateInput = (e) => {
+    setTodoTitle((val) => ({
+      ...val,
+      title: e.target.value,
+    }));
+  };
+  const changeTitle = (e) => {
+    setTitle(e.target.value);
+  };
+
+  return (
+    <>
+      <form className={style.form} onSubmit={addTodoHUndler}>
+        <input
+          type="text"
+          placeholder="Add your todo"
+          value={title}
+          onChange={changeTitle}
+        />
+
+        <button type="submit">
+          <IoMdAddCircle className={style.add} />
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default TodoInput;
