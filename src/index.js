747ad11f79e7feb8c57813f1471cfc2b816742ff
melashu <meshu102@outlook.com>
2022-09-24 08:35:36 +0300

    fix linter error

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/container.css';
 import { BrowserRouter as Router } from 'react-router-dom';
-
 import App from './compontes/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
