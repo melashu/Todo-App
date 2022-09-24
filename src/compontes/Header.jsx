993d7d4d53c49bbf02fa0623c0127e22e633e217
melashu <meshu102@outlook.com>
2022-09-24 08:36:00 +0300

    ajust formating

--- src/compontes/Header.jsx
+++ src/compontes/Header.jsx
@@ -1,11 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import styles from '../css/Header.module.css';
+import React, { useState, useEffect } from "react";
+import styles from "../css/Header.module.css";
 const Header = () => {
-    return <>
-        <header>
-            <h1 className={styles.title}>Your Todos</h1>
-    </header>
+  return (
+    <>
+      <header>
+        <h1 className={styles.title}>Your Todos</h1>
+      </header>
     </>
-}
+  );
+};
 
 export default Header;
